4b977903f0242b1eb74c2e434744edad2326913f
miss李 <jiushu0228@163.com>
2016-11-09 11:27:00 +0800

    修改

--- Textview精确计算限制行数/ViewController.m
+++ Textview精确计算限制行数/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#define MASLine 3
 @interface ViewController ()<UITextViewDelegate>
 
 @end
@@ -23,6 +23,7 @@
     textview.backgroundColor =[UIColor redColor];
     textview.font =[UIFont systemFontOfSize:25.0];
     textview.delegate = self;
+//    textview.layoutManager.allowsNonContiguousLayout = NO;
 //    NSLog(@"%@",NSStringFromUIEdgeInsets(textview.textContainerInset));
    
     textview.textContainerInset = UIEdgeInsetsMake(8, 0, 8, 0);
@@ -38,17 +39,19 @@
     CGFloat count = (labelHeight) / textView.font.lineHeight;
    
   
-    if (count  < 5) {
+    if (count  < MASLine) {
         textView.frame = CGRectMake(0, 64, 350, count *textView.font.lineHeight +16);
+     
 
     }else
     {
-        textView.frame = CGRectMake(0, 64, 350, 5 *textView.font.lineHeight);
+        textView.frame = CGRectMake(0, 64, 350, MASLine *textView.font.lineHeight);
 
         
     }
+//     [textView setContentOffset:CGPointMake(0.0f, (textView.contentSize.height - textView.frame.size.height) / 2) animated:YES];
     [textView scrollRangeToVisible:NSMakeRange(textView.text.length, 1)];
-   
+ 
    
    
 
