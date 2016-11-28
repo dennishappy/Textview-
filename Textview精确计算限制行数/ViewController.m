8baa83d29e40d8e7d046f7ea2c7eba2c3dae0359
miss李 <jiushu0228@163.com>
2016-11-28 09:12:04 +0800

    加上上下间距

--- Textview精确计算限制行数/ViewController.m
+++ Textview精确计算限制行数/ViewController.m
@@ -13,18 +13,20 @@
 @end
 
 @implementation ViewController
-
+{
+    CGFloat masH;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
     
-    UITextView *textview =[[UITextView alloc]initWithFrame:CGRectMake(0, 64, 350, 46)];
+    UITextView *textview =[[UITextView alloc]initWithFrame:CGRectMake(0, 64, 350, 39)];
     textview.backgroundColor =[UIColor redColor];
     textview.font =[UIFont systemFontOfSize:19.0];
     textview.delegate = self;
-//    NSLog(@"%@",NSStringFromUIEdgeInsets(textview.textContainerInset));
-   
+    //    NSLog(@"%@",NSStringFromUIEdgeInsets(textview.textContainerInset));
+    
     textview.textContainerInset = UIEdgeInsetsMake(8, 0, 8, 0);
     [self.view addSubview:textview];
     
@@ -36,25 +38,27 @@
     CGFloat labelHeight = textView.contentSize.height - 16;
     
     CGFloat count = (labelHeight) / textView.font.lineHeight;
-   
-  
-    if (count  <= MASLine) {
+    
+    //    NSLog(@"%f",textView.font.lineHeight);
+    if ((int)count   <= MASLine) {
         textView.frame = CGRectMake(0, 64, 350, textView.contentSize.height);
-     
-
+        [textView setContentOffset:CGPointMake(0, 0) animated:YES];
+        if ((int)count == MASLine) {
+            masH = textView.frame.size.height;
+        }
     }else
     {
-        textView.frame = CGRectMake(0, 64, 350, MASLine *textView.font.lineHeight);
-
-        
+        int del = (int)(count)-MASLine;
+        textView.frame = CGRectMake(0, 64, 350, masH);
+        CGPoint content = CGPointMake(0, 6+ del *textView.font.lineHeight);
+        [textView setContentOffset:content animated:NO];
     }
-//    NSLog(@"%@",NSStringFromCGPoint(textView.contentOffset));
-//     [textView setContentOffset:CGPointMake(0.0f, (textView.contentSize.height - textView.frame.size.height) / 2) animated:YES];
-    [textView scrollRangeToVisible:NSMakeRange(textView.text.length, 1)];
- 
-   
-   
-
+    
+    //    NSLog(@"%@",NSStringFromCGPoint(textView.contentOffset));
+    
+    
+    
+    
 }
 
 @end
